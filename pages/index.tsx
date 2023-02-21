93a88312cf9c6300b7a28f9de7e2568952cf448e
E-V-Castillo <purelyforcode@gmail.com>
2023-02-21 23:36:29 +0800

    development on the front panel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
-/* eslint-disable @next/next/no-img-element */
+import Image from "next/image"
+
 export default function Home() {
   return (
     <>
         <div className="h-screen bg-gray-900">
             <div className="bg-white">
                 <div>
-                    <img src="../public/cup_object.png" alt="" />
-                    <h1 className="">
+                    <div className="">
+                        {/* <img src="/../public/cup.png" alt="cup"/> */}
+                    </div>
+                    <h1>
                         Hello I&#39;m Emmanuel
                     </h1>
                 </div>
@@ -20,6 +23,7 @@ export default function Home() {
             <div className="bg-teal-500">
                 <div className="flex flex-col items-center justify-center gap-5 px-8 pt-10 pb-10">
                         <div className="relative"> 
+
                             <img className="rounded-md w-80" src="https://images.unsplash.com/photo-1676763133944-1738fb369107?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80" alt="blog photo" />
                             <div className="absolute px-5 py-2 text-white -rotate-45 bg-pink-400 -bottom-2 -right-8">
                                 New!
